03db9ec745afdb73d3e9a98cc28d68ba364b57fe
Brian Wheeler <bwheeler96@gmail.com>
2016-04-20 07:53:27 -0700

    specs and shorthand functions for or/and

--- premise.js
+++ premise.js
@@ -84,12 +84,39 @@ function Premise(property) {
     this.orChain.push(this.andChain);
     return matcher;
   };
-  matcher.or.eq = function(val) { return matcher.or().eq(val) };
-  matcher.or.ne = function(val) { return matcher.or().ne(val) };
   matcher.and = function(property) {
     this.andChain.push(property);
     return matcher;
   };
+
+  // I tried creating these shorthands in iteration but got weird errors
+  // TODO clean this up
+  matcher.or.eq        = function(val) { return matcher.or().eq(val)        };
+  matcher.or.ne        = function(val) { return matcher.or().ne(val)        };
+  matcher.or.strictEq  = function(val) { return matcher.or().strictEq(val); };
+  matcher.or.strictNe  = function(val) { return matcher.or().strictNe(val); };
+  matcher.or.gt        = function(val) { return matcher.or().gt(val);       };
+  matcher.or.gte       = function(val) { return matcher.or().gte(val);      };
+  matcher.or.lt        = function(val) { return matcher.or().lt(val);       };
+  matcher.or.lte       = function(val) { return matcher.or().lte(val);      };
+  matcher.or.add       = function(val) { return matcher.or().add(val);      };
+  matcher.or.sub       = function(val) { return matcher.or().sub(val);      };
+  matcher.or.mul       = function(val) { return matcher.or().mul(val);      };
+  matcher.or.div       = function(val) { return matcher.or().div(val);      };
+  matcher.or.mod       = function(val) { return matcher.or().mod(val);      };
+  matcher.and.eq       = function(val) { return matcher.and().eq(val)        };
+  matcher.and.ne       = function(val) { return matcher.and().ne(val)        };
+  matcher.and.strictEq = function(val) { return matcher.and().strictEq(val); };
+  matcher.and.strictNe = function(val) { return matcher.and().strictNe(val); };
+  matcher.and.gt       = function(val) { return matcher.and().gt(val);       };
+  matcher.and.gte      = function(val) { return matcher.and().gte(val);      };
+  matcher.and.lt       = function(val) { return matcher.and().lt(val);       };
+  matcher.and.lte      = function(val) { return matcher.and().lte(val);      };
+  matcher.and.add      = function(val) { return matcher.and().add(val);      };
+  matcher.and.sub      = function(val) { return matcher.and().sub(val);      };
+  matcher.and.mul      = function(val) { return matcher.and().mul(val);      };
+  matcher.and.div      = function(val) { return matcher.and().div(val);      };
+  matcher.and.mod      = function(val) { return matcher.and().mod(val);      };
   return matcher;
 };
 
